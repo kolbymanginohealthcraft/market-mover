15f145d49a1b6dc343adf1c2f6e29bdfbf1d3121
Kolby Mangino <kolbymanginohealthcraft@users.noreply.github.com>
2025-09-04 15:28:04 -0500

    Search behavior not working in prod

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from 'react-router-dom'
 import 'leaflet/dist/leaflet.css'
 import './base.css'; // ✅ Make sure this is here
-
+import './globalSearchBehavior.js'; // Global search behavior for all search inputs
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
